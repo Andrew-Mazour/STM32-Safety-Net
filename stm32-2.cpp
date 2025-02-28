d562d2e9c0527b89071b8c4df72e4622c6b939a5
Andrew-Mazour <andrew.mazour@gmail.com>
2025-02-28 13:29:28 -0500

    Update stm32-2.cpp

--- stm32-2.cpp
+++ stm32-2.cpp
@@ -1,17 +1,16 @@
 // Alarm/Light Board
-"""
-    Receives data from other stm32 to activate
-
-"""
+/*
+    Receives data from another STM32 to activate the alarm
+*/
 
 #include "main.h"
 #include <string.h>
 #include <stdio.h>
 
-#define BUZZER_PIN GPIO_PIN_6  
-#define BUZZER_PORT GPIOB
-#define LED_PIN GPIO_PIN_5     
-#define LED_PORT GPIOB
+#define BUZZER_PIN   GPIO_PIN_6  
+#define BUZZER_PORT  GPIOB
+#define LED_PIN      GPIO_PIN_5     
+#define LED_PORT     GPIOB
 
 UART_HandleTypeDef huart2;
 char received_data[10];
@@ -31,22 +30,23 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
     if (huart->Instance == USART2 && strstr(received_data, "ALERT")) {
         activate_alarm();
     }
-    HAL_UART_Receive_IT(&huart2, (uint8_t *)received_data, 6);
+    HAL_UART_Receive_IT(&huart2, (uint8_t *)received_data, sizeof(received_data));
 }
 
-int main(void) {
-    HAL_Init();
+void GPIO_Init() {
     __HAL_RCC_GPIOB_CLK_ENABLE();
-    __HAL_RCC_USART2_CLK_ENABLE();
 
     GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-    // LED and Buzzer Outputs
+    // Configure LED and Buzzer as outputs
     GPIO_InitStruct.Pin = LED_PIN | BUZZER_PIN;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
     HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+}
 
-    // UART Config
+void UART_Init() {
+    __HAL_RCC_USART2_CLK_ENABLE();
+
     huart2.Instance = USART2;
     huart2.Init.BaudRate = 9600;
     huart2.Init.WordLength = UART_WORDLENGTH_8B;
@@ -54,11 +54,17 @@ int main(void) {
     huart2.Init.Parity = UART_PARITY_NONE;
     huart2.Init.Mode = UART_MODE_TX_RX;
     HAL_UART_Init(&huart2);
+}
+
+int main(void) {
+    HAL_Init();
+    GPIO_Init();
+    UART_Init();
 
     // Start receiving data
-    HAL_UART_Receive_IT(&huart2, (uint8_t *)received_data, 6);
+    HAL_UART_Receive_IT(&huart2, (uint8_t *)received_data, sizeof(received_data));
 
     while (1) {
-        // Nothing needed here, interrupt handles alerts
+        // Main loop remains empty since interrupts handle alerts
     }
 }
